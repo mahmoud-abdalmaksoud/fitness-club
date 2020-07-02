1645b69c9c36144dcbb4702e0a54d6f3f526d089
mahmoudabdalmaksoud <55743511+mahmoud-abdalmaksoud@users.noreply.github.com>
2020-07-02 19:05:59 +0200

    Update MembershipManagement.java

--- MembershipManagement.java
+++ MembershipManagement.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaproject;
 import java.util.InputMismatchException;
 import java.util.LinkedList;
 import java.util.Scanner;
 
 
-/**
- *
- * @author Jamie
- */
+
 public class MembershipManagement {
     
     final private Scanner reader = new Scanner(System.in);
