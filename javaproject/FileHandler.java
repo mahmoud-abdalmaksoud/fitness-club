9ff39e1ddd0464d53f7ade2cb75bb46b9b808500
mahmoudabdalmaksoud <55743511+mahmoud-abdalmaksoud@users.noreply.github.com>
2020-07-02 19:03:55 +0200

    Update FileHandler.java

--- javaproject/FileHandler.java
+++ javaproject/FileHandler.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaproject;
 
 import java.util.LinkedList;
 import java.io.*;
 
-/**
- *
- * @author Jamie
- */
+
 public class FileHandler {
     
     public LinkedList<Member> readFile(){
